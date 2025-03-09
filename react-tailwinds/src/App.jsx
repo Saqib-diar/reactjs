9c5d627df88e8737620d11aa3e646285f0f0ec4e
Saqib Diar <saqib.diar@bcp.com.pk>
2025-03-10 00:57:52 +0500

    with tailwind configuration

---
+++ react-tailwinds/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='flex justify-center -space-x-96'>
+      <div>
+        <p className=''>lift side</p>
+      </div>
+      <div>
+        <p>right side</p>
+      </div>
+    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
